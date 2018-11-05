ec9212af00e7b3b3cf9dbff57270a78449e6cb0c
jgi65790 <jgi65790@gmail.com>
2018-11-06 00:45:51 +0900

    테스트213123

--- 4344_평균은넘겠지.cpp
+++ 4344_평균은넘겠지.cpp
@@ -5,23 +5,19 @@ using namespace std;
 int main()
 {
 	int c;
-	scanf_s("%d", &c, sizeof(c));
-
 	int n;
+	scanf("%d", &c, sizeof(c));
 
 	for (int i = 0; i < c; i++) {
+		int sum = 0;
+		double avg = 0;
+		int a = 0;
 		scanf("%d", &n);
 		int* arr = new int[n];
 		for (int j = 0; j < n; j++) {
 			scanf("%d", &arr[j]);
+			sum += arr[j];
 		}
-		int sum = 0;
-		double avg = 0;
-		int a = 0;
-		for (int k = 0; k < n; k++) {
-			sum += arr[k];
-		}
-
 		avg = (double)sum / n;
 		for (int m = 0; m < n; m++) {
 			if (arr[m] > avg) {
@@ -31,7 +27,6 @@ int main()
 		double output = (double)a / n * 100;
 		printf("%.3f%%\n", output);
 	}
-
 	return 0;
 }
 
